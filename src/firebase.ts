ea638b91e496984d31ad69510ce2e9d2045a9448
nadircelebic <82423067+nadircelebic@users.noreply.github.com>
2025-08-15 00:46:54 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { getStorage, setMaxUploadRetryTime } from 'firebase/storage'
 
 
 // Your web app's Firebase configuration
@@ -20,4 +20,5 @@ export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
 
+setMaxUploadRetryTime(storage, 10 * 60 * 1000);
 
